ca21a09fabec665cf837ec0cfe53ca69006be38c
Valentin LEPILLER <vlepille@student.42.fr>
2023-02-27 18:18:03 +0100

    Add some comment test lines in main

--- main.c
+++ main.c
@@ -20,6 +20,25 @@ int	main(void)
 
 	put_origins(&renderer);
 	draw_cube(&renderer, (t_point){20., -20., -20., 0x00FFFF00}, 50.);
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){200, -200, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){200, 0, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){200, -50, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){200, 50, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){200, 200, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){0, 200, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){50, 200, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){-50, 200, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){-200, 200, 0, -1});
+
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){-200, 200, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){-200, 0, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){-200, 50, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){-200, -50, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){-200, -200, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){0, -200, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){50, -200, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){-50, -200, 0, -1});
+	//draw_line(&renderer, (t_point){0, 0, 0, -1}, (t_point){-200, -200, 0, -1});
 	render_tick(&renderer);
 
 	mlx_hook(renderer.window, 17, 0, hook_destroy, &renderer);
