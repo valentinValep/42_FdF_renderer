21f7d4214834a83bc97d5479bbf66024c3017517
Valentin LEPILLER <vlepille@student.42.fr>
2023-03-07 15:54:38 +0100

    Add some line to test smooth colors

--- main.c
+++ main.c
@@ -19,29 +19,29 @@ int	main(void)
 	}
 
 	put_origins(&renderer);
-	draw_line(&renderer, (t_point){-40, 700000, 0, -1}, (t_point){60, -10000000, 0, -1});
-	draw_line(&renderer, (t_point){-50, 700000, 0, -1}, (t_point){70, -10000000, 0, -1});
-	draw_line(&renderer, (t_point){-60, 700000, 0, -1}, (t_point){80, -10000000, 0, -1});
-	draw_line(&renderer, (t_point){-140, 700000, 0, -1}, (t_point){60, -10000000, 0, -1});
-	draw_line(&renderer, (t_point){-150, 700000, 0, -1}, (t_point){70, -10000000, 0, -1});
-	draw_line(&renderer, (t_point){-160, 700000, 0, -1}, (t_point){80, -10000000, 0, -1});
-	draw_line(&renderer, (t_point){-400, 700, 0, -1}, (t_point){600, -700, 0, -1});
-	draw_line(&renderer, (t_point){-500, 700, 0, -1}, (t_point){500, -700, 0, -1});
-	draw_line(&renderer, (t_point){-600, 700, 0, -1}, (t_point){400, -700, 0, -1});
-	draw_line(&renderer, (t_point){-700, 700, 0, -1}, (t_point){300, -700, 0, -1});
-	draw_line(&renderer, (t_point){-800, 700, 0, -1}, (t_point){200, -700, 0, -1});
-	draw_line(&renderer, (t_point){-450, 700, 0, -1}, (t_point){600, -700, 0, -1});
-	draw_line(&renderer, (t_point){-550, 700, 0, -1}, (t_point){500, -700, 0, -1});
-	draw_line(&renderer, (t_point){-650, 700, 0, -1}, (t_point){400, -700, 0, -1});
-	draw_line(&renderer, (t_point){-750, 700, 0, -1}, (t_point){300, -700, 0, -1});
-	draw_line(&renderer, (t_point){-850, 700, 0, -1}, (t_point){200, -700, 0, -1});
-	draw_line(&renderer, (t_point){-470, 700, 0, -1}, (t_point){600, -700, 0, -1});
-	draw_line(&renderer, (t_point){-570, 700, 0, -1}, (t_point){500, -700, 0, -1});
-	draw_line(&renderer, (t_point){-670, 700, 0, -1}, (t_point){400, -700, 0, -1});
-	draw_line(&renderer, (t_point){-770, 700, 0, -1}, (t_point){300, -700, 0, -1});
-	draw_line(&renderer, (t_point){-870, 700, 0, -1}, (t_point){200, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-40, 700000, 0, -1}, (t_point){60, -10000000, 0, -1});
+	//draw_line(&renderer, (t_point){-50, 700000, 0, -1}, (t_point){70, -10000000, 0, -1});
+	//draw_line(&renderer, (t_point){-60, 700000, 0, -1}, (t_point){80, -10000000, 0, -1});
+	//draw_line(&renderer, (t_point){-140, 700000, 0, -1}, (t_point){60, -10000000, 0, -1});
+	//draw_line(&renderer, (t_point){-150, 700000, 0, -1}, (t_point){70, -10000000, 0, -1});
+	//draw_line(&renderer, (t_point){-160, 700000, 0, -1}, (t_point){80, -10000000, 0, -1});
+	//draw_line(&renderer, (t_point){-400, 700, 0, -1}, (t_point){600, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-500, 700, 0, -1}, (t_point){500, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-600, 700, 0, -1}, (t_point){400, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-700, 700, 0, -1}, (t_point){300, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-800, 700, 0, -1}, (t_point){200, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-450, 700, 0, -1}, (t_point){600, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-550, 700, 0, -1}, (t_point){500, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-650, 700, 0, -1}, (t_point){400, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-750, 700, 0, -1}, (t_point){300, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-850, 700, 0, -1}, (t_point){200, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-470, 700, 0, -1}, (t_point){600, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-570, 700, 0, -1}, (t_point){500, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-670, 700, 0, -1}, (t_point){400, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-770, 700, 0, -1}, (t_point){300, -700, 0, -1});
+	//draw_line(&renderer, (t_point){-870, 700, 0, -1}, (t_point){200, -700, 0, -1});
 
-	draw_cube(&renderer, (t_point){20., -20., -20., 0x00FFFF00}, 50.);
+	//draw_cube(&renderer, (t_point){20., -20., -20., 0x00FFFF00}, 50.);
 	draw_line(&renderer, (t_point){0, 0, 0, 0x00FF0000}, (t_point){200, -200, 0, 0x00FF7F00});
 	draw_line(&renderer, (t_point){0, 0, 0, 0x00FF7F00}, (t_point){200, 0, 0, 0x00FFFF00});
 	draw_line(&renderer, (t_point){0, 0, 0, 0x00FFFF00}, (t_point){200, -50, 0, 0x007FFF00});
@@ -61,6 +61,10 @@ int	main(void)
 	draw_line(&renderer, (t_point){0, 0, 0, 0x00808080}, (t_point){50, -200, 0, 0x00C0C0C0});
 	draw_line(&renderer, (t_point){0, 0, 0, 0x00C0C0C0}, (t_point){-50, -200, 0, 0x00FFFFFF});
 	draw_line(&renderer, (t_point){0, 0, 0, 0x00FFFFFF}, (t_point){-200, -200, 0, 0x00FF0000});
+	draw_line(&renderer, (t_point){-400, 400, 300, 0x00FFFFFF}, (t_point){-300, 300, 300, 0x00FF0000});
+	draw_line(&renderer, (t_point){-300, 300, 300, 0x00FF0000}, (t_point){-200, 200, 300, 0x00FF00FF});
+	draw_line(&renderer, (t_point){-200, 200, 300, 0x00FF00FF}, (t_point){-100, 100, 300, 0x00FFFF00});
+	draw_line(&renderer, (t_point){-100, 100, 300, 0x00FFFF00}, (t_point){0, 0, 300, 0x0000FF00});
 	render_tick(&renderer);
 
 	mlx_hook(renderer.window, 17, 0, hook_destroy, &renderer);
